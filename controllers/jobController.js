318e3ac70d7a9c5833d54a98f0d24fd0139654c5
Daniel001-odii <xenithheight@gmail.com>
2023-10-15 20:08:29 +0100

    updates

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -31,7 +31,7 @@ exports.postJob = async (req, res) => {
     // The user is an employer, allow them to post a job
     try {
       const { job_title, job_description, skills, period, budget_type, budget, location } = req.body;
-      
+
       // Validate the request body
       if (!job_title) {
         return res.status(400).json({ message: 'Job title is required' });
@@ -51,7 +51,7 @@ exports.postJob = async (req, res) => {
       if (!location) {
         return res.status(400).json({ message: 'job location type is required' });
       }
-      
+
       const job = new Job({
         job_title,
         job_description,
@@ -63,9 +63,22 @@ exports.postJob = async (req, res) => {
         location,
         // Add other job fields here
       });
-      
-      await job.save();
-      
+
+      await job.save((err, job) => {
+          // Update the employer's document with the posted job's reference
+          Employer.findByIdAndUpdate(
+            req.employerId, // Replace with the actual employer's ID
+            { $push: { jobs: job._id } },
+            (updateErr) => {
+              if (updateErr) {
+                // Handle the update error
+              } else {
+                // The employer's document is now updated with the posted job
+              }
+            }
+          );
+      });
+
       res.status(201).json({ message: 'Job posted successfully' });
     } catch (error) {
       res.status(500).json({ message: 'Error posting job', error: error.message });
@@ -80,7 +93,7 @@ exports.postJob = async (req, res) => {
 exports.listJobs = async (req, res) => {
   try {
     const jobs = await Job.find();
-    
+
     res.status(200).json({ jobs });
   } catch (error) {
     res.status(500).json({ message: 'Error listing jobs', error: error.message });
@@ -92,7 +105,7 @@ exports.listJobs = async (req, res) => {
 exports.getJobById = async (req, res) => {
   try {
     const jobId = req.params.jobId; // Get the job ID from the URL parameter
-    
+
     // Query the database to find the job by its ID
     const job = await Job.findById(jobId);
 
@@ -266,10 +279,10 @@ exports.getSavedJobs = (req, res) => {
 exports.searchJobs = async (req, res) => {
   try {
     const { keywords, budgetMin, budgetMax, jobType, location, posted } = req.query;
-    
+
     // Build the filter criteria based on user's input
     const filter = {};
-    
+
     // Search keywords in job title and description
     if (keywords) {
       filter.$or = [
@@ -277,7 +290,7 @@ exports.searchJobs = async (req, res) => {
         { job_description: { $regex: keywords, $options: 'i' } },
       ];
     }
-    
+
     // Filter by budget range
     if (budgetMin && budgetMax) {
       filter.budget = { $gte: parseInt(budgetMin), $lte: parseInt(budgetMax) };
@@ -286,17 +299,17 @@ exports.searchJobs = async (req, res) => {
     } else if (budgetMax) {
       filter.budget = { $lte: parseInt(budgetMax) };
     }
-    
+
     // Filter by job type
     if (jobType) {
       filter.period = jobType;
     }
-    
+
     // Filter by location
     if (location) {
       filter.location = { $regex: location, $options: 'i' };
     }
-    
+
     // Calculate date range based on "posted" value
     if (posted) {
       const currentDate = new Date();
@@ -333,46 +346,6 @@ exports.searchJobs = async (req, res) => {
   }
 };
 
-
-exports.applyForJo = async (req, res) => {
-  try {
-    const { jobId, coverLetter, attachment, counterOffer, reasonForCounterOffer } = req.body;
-    const token = req.headers.authorization.split(' ')[1]; // Get the JWT token from the request headers
-
-    // Verify the token and get the user ID from it
-    jwt.verify(token, process.env.API_SECRET, async (err, decoded) => {
-      if (err) {
-        return res.status(401).json({ message: 'Unauthorized' });
-      }
-
-      const userId = decoded.id; // Get the user's ID from the token
-
-      // Create a job application object
-      const application = {
-        user: userId,
-        coverLetter,
-        attachment,
-        counterOffer,
-        reasonForCounterOffer,
-      };
-
-      // Find the job by ID and add the application to the applications array
-      const job = await Job.findByIdAndUpdate(
-        jobId,
-        { $push: { applications: application } },
-        { new: true }
-      );
-
-      if (!job) {
-        return res.status(404).json({ message: 'Job not found' });
-      }
-
-      res.status(201).json({ message: 'Application submitted successfully' });
-    });
-  } catch (error) {
-    res.status(500).json({ message: 'Error submitting application', error: error.message });
-  }
-};
 
 exports.applyForJob = async (req, res) => {
   try {
@@ -521,53 +494,6 @@ exports.deleteJob = async (req, res) => {
 };
 
 
-exports.hireApp = async (req, res) => {
-  try {
-    const { jobId, applicantId } = req.params; // Get the job ID and applicant ID from the route parameters
-    const token = req.headers.authorization.split(' ')[1]; // Get the JWT token from the request headers
-
-    // Verify the token and get the user ID and role from it
-    jwt.verify(token, process.env.API_SECRET, async (err, decoded) => {
-      if (err) {
-        return res.status(401).json({ message: 'Unauthorized' });
-      }
-
-      const userId = decoded.id; // Get the user's ID from the token
-      const userRole = decoded.role; // Get the user's role from the token
-
-      if (userRole !== 'employer') {
-        return res.status(403).json({ message: 'Access denied. Only employers can hire applicants.' });
-      }
-
-
-
-
-      // Find the job by ID and check if it was posted by the employer making the request
-      const job = await Job.findOne({ _id: jobId, createdBy: userId });
-
-      if (!job) {
-        return res.status(404).json({ message: 'Job not found or you do not have permission to hire for it.' });
-      }
-
-      // Check if the applicant exists in the job's applications
-      const applicantIndex = job.applications.findIndex((app) => app.user.toString() === applicantId);
-
-      if (applicantIndex === -1) {
-        return res.status(404).json({ message: 'Applicant not found for this job.' });
-      }
-
-      // Mark the applicant as hired
-      job.applications[applicantIndex].status = 'hired';
-
-      // Save the updated job document
-      await job.save();
-
-      res.status(200).json({ message: 'Applicant hired successfully' });
-    });
-  } catch (error) {
-    res.status(500).json({ message: 'Error hiring applicant', error: error.message });
-  }
-};
 
 
 exports.hireApplicant = async (req, res) => {
@@ -583,21 +509,37 @@ exports.hireApplicant = async (req, res) => {
       }
       const employerId = decoded.id; // Get the employer's ID from the token
 
-      const message = `You Successfully hired ${userId}`;
+      // alert both user and employer on successful hire.....
+      const notifiedUser = await User.findOne({_id: userId});
+      const message = `You Successfully hired ${notifiedUser.firstname} ${notifiedUser.lastname}`;
       const notification = new Notification({
         recipientId: employerId,
         recipientModel: 'Employer',
-        message,
+        message: message,
       });
-        // Save the notification to the user's/employer's notifications array
       notification.save();
       const employer = await Employer.findOne({_id: employerId});
       if(employer){
-        console.log("employer found!")
         employer.notifications.push(notification);
         employer.save();
       }
-      
+
+      const hiredJob = await Job.findOne({_id: jobId});
+      const RecipientMessage = `Congratulations, youve been hired for the job: ${hiredJob.job_title}`;
+      const notification2 = new Notification({
+        recipientId: userId,
+        recipientModel: 'User',
+        message: RecipientMessage,
+      });
+        // Save the notification to the user's/employer's notifications array
+      notification2.save();
+      const user = await User.findOne({_id: userId});
+      if(user){
+        user.notifications.push(notification2);
+        user.save();
+      }
+
+
 
 
       // Check if the employer has the permission to hire for this job (e.g., they posted the job)
@@ -630,7 +572,7 @@ exports.getJobsByEmployer = (req, res) => {
     if (err) {
       return res.status(500).json({ message: 'Error retrieving jobs', error: err.message });
     }
-    
+
     res.status(200).json({ jobs });
   });
 };
