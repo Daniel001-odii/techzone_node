4495d2a092b37b44b2113e9107c01815c9023573
Odii Daniel <xenithheight@gmail.com>
2024-06-05 08:29:18 +0100

    email sender now working

--- utils/email.js
+++ utils/email.js
@@ -3,42 +3,66 @@
 **
 EMAIL CONCERNED CONTROLLERS
 **/
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+require("dotenv").config()
+ const nodemailer = require("nodemailer");
+ const { google } = require("googleapis");
+ const OAuth2 = google.auth.OAuth2;
 
-// const transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: process.env.GMAIL_USER,
-//     pass: process.env.GMAIL_PASS,
-//   },
-// });
+ const createTransporter = async () => {
+  try {
+    const oauth2Client = new OAuth2(
+        process.env.CLIENT_ID,
+        process.env.CLIENT_SECRET,
+        "https://developers.google.com/oauthplayground"
+      );
 
-const transporter = nodemailer.createTransport({
-  host: process.env.MAILTRAP_HOST,
-  port: process.env.MAILTRAP_PORT,
-  auth: {
-    user: process.env.MAILTRAP_USER,
-    pass: process.env.MAILTRAP_PASS,
-  },
-});
+      oauth2Client.setCredentials({
+        refresh_token: process.env.REFRESH_TOKEN,
+      });
+
+      const accessToken = await new Promise((resolve, reject) => {
+        oauth2Client.getAccessToken((err, token) => {
+          if (err) {
+            console.log("*ERR: ", err)
+            reject();
+          }
+          resolve(token); 
+        });
+      });
+
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          type: "OAuth2",
+          user: process.env.GMAIL_USER,
+          accessToken,
+          clientId: process.env.CLIENT_ID,
+          clientSecret: process.env.CLIENT_SECRET,
+          refreshToken: process.env.REFRESH_TOKEN,
+        },
+      });
+      return transporter;
+  } catch (err) {
+    return err
+  }
+};
 
 const sendEmail = async (to, subject, text, html) => {
   try {
     const mailOptions = {
+      // from: process.env.GMAIL_USER,
       from: "noreply@apexteks.com",
       to,
       subject,
       text,
       html,
-    };
+    }
 
-    const info = await transporter.sendMail(mailOptions);
-    console.log('Email sent: ' + info.response);
-    return info;
-  } catch (error) {
-    console.error('Error sending email: ', error);
-    throw error;
+    let emailTransporter = await createTransporter();
+    await emailTransporter.sendMail(mailOptions);
+    console.log("email sent: ", emailTransporter.response);
+  } catch (err) {
+    console.log("error sending email: ", err)
   }
 };
 
