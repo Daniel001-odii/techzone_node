4f9c28a5d3d8350243555824f08b3832d3b408cc
Odii Daniel <xenithheight@gmail.com>
2024-06-05 11:20:17 +0100

    add again

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const mongoose = require('mongoose');
 // Use the cors middleware with options to specify the allowed origin [----DO NOT REMOVE FRPM HERE----]
 // app.use(cors());
 app.use(cors({
-  origin: ['http://localhost:8080', 'http://localhost:3000', 'https://tech-zone-navy.vercel.app', 'https://www.apexteks.com'], // Replace this with the origin of your frontend application
+  origin: ['http://localhost:8080', 'https://www.apexteks.com', 'https://tech-zone-navy.vercel.app', 'https://www.apexteks.com'], // Replace this with the origin of your frontend application
   credentials: true // Allow sending cookies with the CORS request
 }));
 
